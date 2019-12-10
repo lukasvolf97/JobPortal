783245a2305248ecd8e1e2e15cf7f8d9fb4e1d4a
lukas.volf97 <lukas.volf97@gmail.com>
2019-12-10 17:34:12 +0100

    Update fix

--- PresentationLayer/Controllers/JobseekerController.cs
+++ PresentationLayer/Controllers/JobseekerController.cs
@@ -27,6 +27,29 @@ namespace PresentationLayer.Controllers
             return View("JobseekerProfileView", model);
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Index(JobseekerProfileModel jobseeker)
+        {
+            var userJobseeker = await jobseekerFacade.GetUserAccordingToUsernameAsync(User.Identity.Name.Split('@')[0]);
+
+            var jobseekerTmp = new JobseekerDTO()
+            {
+                Id = userJobseeker.Id,
+                FirstName = jobseeker.FirstName,
+                LastName = jobseeker.LastName,
+                TitlesAfterName = jobseeker.TitlesAfterName,
+                TitlesBeforeName = jobseeker.TitlesBeforeName,
+                Address = jobseeker.Address,
+                MobilePhoneNumber = jobseeker.MobilePhoneNumber,
+                Username = userJobseeker.Username,
+                PasswordHash = userJobseeker.PasswordHash,
+                PasswordSalt = userJobseeker.PasswordSalt
+            };
+            await jobseekerFacade.UpdateJobOffer(jobseekerTmp);
+            var model = InitializeModel(await jobseekerFacade.GetJobseekerById(userJobseeker.Id));
+            return  View("JobseekerProfileView", model);
+        }
+
         private JobseekerProfileModel InitializeModel(JobseekerDTO jobseeker)
         {
             return new JobseekerProfileModel
